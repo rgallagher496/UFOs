4ff8cb811c7fa2d52c6023402519c51cf68b616b
Robert Gallagher <rgallagher496@gmail.com>
2021-11-12 14:59:58 -0500

    Added functions to build filtered table based on user input

--- static/js/app.js
+++ static/js/app.js
@@ -16,4 +16,23 @@ function buildTable(data) {
             cell.text(val);
         });
     });
-}
+};
+
+function handleClick() {
+    //gets datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    //check to see if a date was entred and filter the data using that date
+    if (date) {
+        //applying filter to data
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    //calling funtion used to build table from filtered data
+    buildTable(filteredData);
+};
+
+//listens for button click and builds filtered table using handleClick function above
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+//loads full data set to table when page originally loads
+buildTable(tableData);
